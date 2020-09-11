3df60cd77fa55e9128ec957531421cb8c454038b
Denis Zenkin <astron4ik.94@gmail.com>
2020-09-11 18:37:33 +0300

    Update AutoPrinterReports.py

--- AutoPrinterReports.py
+++ AutoPrinterReports.py
@@ -8,9 +8,9 @@
 path - путь до рабочего файла
 path_logfile - путь до файла с логами
 path_copy - путь, куда копируется заполненый файл
-zabbix_server_url - адрес мониторинга Zabbix
-zabbix_login - Логин пользователя Zabbix
-zabbix_password - Пароль пользователя Zabbix
+zabbix_server_url - адрес мониторинга Zabbix по WEB API
+zabbix_login - Логин пользователя Zabbix по WEB API
+zabbix_password - Пароль пользователя Zabbix по WEB API
 
 @ Автор скрипта:
 Зенкин Денис
@@ -27,12 +27,12 @@ from openpyxl.utils import get_column_letter
 from pyzabbix import ZabbixAPI
 
 # Настраиваемые переменные
-path = "C:\\Users\\<UserName>\\PycharmProjects\\<ProjectName>\\Отчет_принтеры.xlsx"
-path_logfile = "C:\\Users\\<UserName>\\PycharmProjects\\<ProjectName>\\logfile.txt"
-zabbix_server_url = "http://url"
-zabbix_login = ""
-zabbix_password = ""
-path_copy = ""
+path = "<path>\\Ежедневный_отчет_по_принтерам.xlsx"
+path_logfile = "<path>\\logfile.txt"
+zabbix_server_url = "http://<url>"
+zabbix_login = "<login>"
+zabbix_password = "<password>"
+path_copy = ""  # Копирование пока отключено
 
 
 def log(text):
@@ -49,6 +49,7 @@ def data():
         Определение координат последней записи.
         Цикл проверяет столбец A, находит номер следующей пустой строки и записывает туда текущую дату.
     """
+    global coord
     for i in range(1, 1000):
         y = sheet_ranges.cell(row=i, column=1).value
         if i > 7 and (y is None):
@@ -79,9 +80,10 @@ def printers(group):
         Получает и записывает значения serial и total pages для каждого принтера.
         Записывает в excel.
     """
-    hosts = z.host.get(groupids=group, output=['hostid', 'name'])  # Группа Принтеры Офис
+    hosts = z.host.get(groupids=group, output=['hostid', 'name', 'host'])  # Группа Принтеры Офис
     for host in hosts:
         name = host['name']  # Имя узла принтера
+        ip = host['host']
         # print(host['hostid'],host['name'])
         host = host.get('hostid')
         pages = int(total_pages_and_serial(host, 6))  # Узнаем Кол-во Страниц, 6 это номер позиции total pages
@@ -93,10 +95,10 @@ def printers(group):
                     column = cell.column_letter
                     coord_pages = cell.column_letter + coord
                     coord_name = cell.column_letter + "4"  # Координаты строки места принтера
-                    coord_ip = cell.column_letter + "3"
+                    coord_ip = cell.column_letter + "3"  # Координаты строки ip адреса
                     sheet_ranges[coord_name] = name
                     sheet_ranges[coord_pages] = pages
-                    sheet_ranges[coord_ip] = ip(name)
+                    sheet_ranges[coord_ip] = str(ip)  # Записываем IP
                     # print(serial, ' - ', pages)
                     dict_excel[cell.value] = column
                 elif cell.value != serial:  # Создает словарь из серийников, для сравнения
@@ -129,16 +131,6 @@ def row_1(row):
     return row2
 
 
-def ip(name):
-    """
-        Определяет ip адрес.
-        В имени файла последние 4 цифры ip адреса.
-    """
-    ip = name[-4:-1]
-    ip = "192.168.15." + ip
-    return ip
-
-
 date = datetime.datetime.today().strftime("%d.%m.%y")  # Текущая дата
 
 file = open(path_logfile, "a")
@@ -150,7 +142,6 @@ file.write("\n\n")
 file.close()
 
 serial_zabbix = []
-dict_excel = {}
 
 try:
     wb = load_workbook(path)  # Загружаем файл
@@ -165,57 +156,60 @@ try:
 except OSError:
     log("Нет соединения с сервером Zabbix\n\n")
     raise
-sheet_ranges = wb['Данные']  # Перешли в книгу "Данные"
-group_office = 45
-group_sklad = 44
-coord = data()
 
-printers(group_office)
-printers(group_sklad)
+dist_book = {'Офис': 45, 'Склад': 44, 'ОСП': 42, 'Москва': 61}
 
-coord3 = "A" + coord
-coord4 = "DR" + coord
-summa_prom = []
-summa = []
+for book in dist_book:
+    sheet_ranges = wb[book]
+    summa_prom = []
+    summa = []
+    dict_excel = {}
+    coord = data()
+    coord3 = "A" + coord
+    coord4 = "DR" + coord
+    printers(dist_book[book])
 
-for cellObj in sheet_ranges[coord3:coord4]:
-    for cell in cellObj:
-        coordinate = cell.coordinate
-        row = cell.row  # текущий ряд ячейки
-        row_2 = row_1(row)  # предыдущий ряд ячейки
-        coord_column1 = cell.column  # текущий столбец ячейки
-        value_5 = sheet_ranges[cell.column_letter + str(5)].value  # Чтение ячейки столбца типа (счетчик и т.п.)
-        if cell.value is None:
-            if value_5 == "Счетчик":
-                coord6 = coord_column1 + int(row_2)
-                value_6 = sheet_ranges[cell.column_letter + str(coord6)].value
-                sheet_ranges[coordinate] = value_6  # Запись в ячейку
-            elif value_5 == "Прирост":
-                summa = raschet(cell, row_2)
-            elif value_5 == "Сумма":
-                for element in summa_prom:
-                    summa.append(element)
-                summa_string = '+'.join(summa)
-                summa_string = "=" + summa_string
-                sheet_ranges[coordinate] = summa_string
-            elif value_5 == "Разница Сумм":
-                raschet(cell, row_2)
+    for cellObj in sheet_ranges[coord3:coord4]:
+        for cell in cellObj:
+            coordinate = cell.coordinate
+            row = cell.row  # текущий ряд ячейки
+            row_2 = row_1(row)  # предыдущий ряд ячейки
+            coord_column1 = cell.column  # текущий столбец ячейки
+            value_5 = sheet_ranges[cell.column_letter + str(5)].value  # Чтение ячейки столбца типа (счетчик и т.п.)
+            if cell.value is None:
+                if value_5 == "Счетчик":
+                    coord6 = coord_column1 + int(row_2)
+                    value_6 = sheet_ranges[cell.column_letter + str(coord6)].value
+                    sheet_ranges[coordinate] = value_6  # Запись в ячейку
+                elif value_5 == "Прирост":
+                    summa = raschet(cell, row_2)
+                elif value_5 == "Сумма":
+                    for element in summa_prom:
+                        summa.append(element)
+                    summa_string = '+'.join(summa)
+                    summa_string = "=" + summa_string
+                    sheet_ranges[coordinate] = summa_string
+                elif value_5 == "Разница Сумм":
+                    raschet(cell, row_2)
 
-# Удаляем запись none, что бы не баговал
-del dict_excel[None]
+    # Удаляем запись none, что бы не баговал
+    del dict_excel[None]
 
-for dict_keys in dict_excel.keys():
-    """
-        Выставляет Резерв в ячейках.
-        Которых нету в zabbix.
-    """
-    if dict_keys not in serial_zabbix:  # Этот серийник не определяется в Zabbix
-        coord7 = dict_excel[dict_keys] + "3"
-        coord8 = dict_excel[dict_keys] + "4"
-        sheet_ranges[coord7] = "Резерв!"
-        sheet_ranges[coord8] = "Резерв!"
+    for dict_keys in dict_excel.keys():
+        """
+            Выставляет Резерв в ячейках.
+            Которых нету в zabbix.
+        """
+        if dict_keys not in serial_zabbix:  # Этот серийник не определяется в Zabbix
+            coord7 = dict_excel[dict_keys] + "3"
+            coord8 = dict_excel[dict_keys] + "4"
+            sheet_ranges[coord7] = "Резерв!"
+            sheet_ranges[coord8] = "Резерв!"
+
+
 try:
     wb.save(path)
+    log("Запись завершена\n\n")
 except PermissionError:
     log("Не возможно сохранить файл excel, у кого-то он открыт! Сохранение в промежуточный файл!\n\n")
     raise
